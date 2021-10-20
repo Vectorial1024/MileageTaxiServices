7f9a98a25284dd358a16aac206a962ae263c78e9
Vectorial1024 <vincentwwy168@gmail.com>
2021-10-20 16:38:32 +0800

    Initial preparation

---
+++ MileageTaxiServices/MileageTaxiServices.cs
@@ -0,0 +1,74 @@
+﻿using CitiesHarmony.API;
+using ICities;
+
+namespace MileageTaxiServices
+{
+    public class MileageTaxiServices : LoadingExtensionBase, IUserMod
+    {
+        public virtual string Name
+        {
+            get
+            {
+                return "Mileage Taxi Services";
+            }
+        }
+
+        public virtual string Description
+        {
+            get
+            {
+                return "Simulate distance-based taxi fares.";
+            }
+        }
+
+        /// <summary>
+        /// Executed whenever a level completes its loading process.
+        /// This mod the activates and patches the game using Hramony library.
+        /// </summary>
+        /// <param name="mode">The loading mode.</param>
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            /*
+             * This function can still be called when loading up the asset editor,
+             * so we have to check where we are right now.
+             */
+
+            switch (mode)
+            {
+                case LoadMode.LoadGame:
+                case LoadMode.NewGame:
+                case LoadMode.LoadScenario:
+                case LoadMode.NewGameFromScenario:
+                    break;
+
+                default:
+                    return;
+            }
+
+            UnifyHarmonyVersions();
+            PatchController.Activate();
+        }
+
+        /// <summary>
+        /// Executed whenever a map is being unloaded.
+        /// This mod then undoes the changes using the Harmony library.
+        /// </summary>
+        public override void OnLevelUnloading()
+        {
+            UnifyHarmonyVersions();
+            PatchController.Deactivate();
+        }
+
+        private void UnifyHarmonyVersions()
+        {
+            if (HarmonyHelper.IsHarmonyInstalled)
+            {
+                // this code will redirect our Harmony 2.x version to the authoritative version stipulated by CitiesHarmony
+                // I will make it such that the game will throw hard error if Harmony is not found,
+                // as per my usual software deployment style
+                // the user will have to subscribe to Harmony by themselves. I am not their parent anyways.
+                // so this block will have to be empty.
+            }
+        }
+    }
+}
